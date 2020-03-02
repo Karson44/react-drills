b11a11afa01dd16a2dab18e2041827703b16b41a
unknown <kccoug44@gmail.com>
2020-03-02 09:55:42 -0700

    complete

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange(value) {
+    this.setState({ message: value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+import "./App.css"
+
+import React, {Component} from 'react';
+
+export default class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      mattBelt: 4000,
+      mattHat: 2,
+    }
+  }
+  mattBelt = () =>{
+    this.setState({
+      mattBelt: this.state.mattBelt / 2
+    })
+  }
+  mattHat = () =>{
+    this.setState({
+      mattHat: this.state.mattHat * 2
+    })
+  }
+  render(){
+    return(
+      <div>
+        <div className="matt" onClick={() => this.mattBelt()}>
+          {this.state.mattBelt}
+        </div>
+        <div className="karson" onClick={() => this.mattHat()}>
+          {this.state.mattHat}
+        </div>
+      </div>
+    )
+  }
+}
